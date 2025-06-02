20f99fbfcb3083dfe896c91fa35bbc3c3f597dee
Eduardo Rosendo <eduardojra96@gmail.com>
2025-06-02 09:49:45 -0400

    feat(cache): Adds custom `set` method

--- django_s3_express_cache/s3_cache.py
+++ django_s3_express_cache/s3_cache.py
@@ -1,5 +1,9 @@
+import pickle
+import struct
+import time
+
 import boto3
-from django.core.cache.backends.base import BaseCache
+from django.core.cache.backends.base import DEFAULT_TIMEOUT, BaseCache
 
 
 class S3ExpressCacheBackend(BaseCache):
@@ -7,3 +11,30 @@ class S3ExpressCacheBackend(BaseCache):
         super().__init__(params)
         self.bucket_name = bucket
         self.client = boto3.client("s3")
+
+    def get_backend_timeout(self, timeout=DEFAULT_TIMEOUT):
+        """
+        Return the timeout value usable by this backend based upon the provided
+        timeout.
+        """
+        if timeout == DEFAULT_TIMEOUT:
+            timeout = self.default_timeout
+        # The key will be made persistent if None used as a timeout.
+        # Non-positive values will cause the key to be deleted.
+        return None if timeout is None else max(0, int(timeout))
+
+    def set(self, key, value, timeout=DEFAULT_TIMEOUT, version=None):
+        """
+        Set a value in the cache. If timeout is given, use that timeout for the
+        key; otherwise use the default cache timeout.
+
+        The value is serialized using pickle and stored along with its
+        expiration time.
+        """
+        key = self.make_and_validate_key(key, version=version)
+
+        timeout = self.get_backend_timeout(timeout)
+        expiration_time = time.time_ns() + timeout * 1e9 if timeout else 0
+
+        content = struct.pack("d", expiration_time) + pickle.dumps(value)
+        self.client.put_object(Bucket=self.bucket_name, Key=key, Body=content)
