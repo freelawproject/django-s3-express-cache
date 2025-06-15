b6471a27dfb8de0a864009240cf77157d9335594
Eduardo Rosendo <eduardojra96@gmail.com>
2025-06-15 18:35:42 -0400

    tests(cache): Adds test suite for helper functions

---
+++ tests/settings.py
@@ -0,0 +1,23 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = "django-s3-express-cache"
+
+ALLOWED_HOSTS = ["*"]
+
+SITE_ID = 1
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    },
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_s3_express_cache.s3_cache.S3ExpressCacheBackend",
+        "LOCATION": "default",
+    },
+}
