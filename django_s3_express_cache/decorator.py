cd2efd8e958ff24763c0113845cf6f1bfd4cc3f1
Eduardo Rosendo <eduardojra96@gmail.com>
2025-12-22 12:30:01 -0400

    feat(decorator): Add time_based_prefix to cache_page decorator

--- django_s3_express_cache/decorator.py
+++ django_s3_express_cache/decorator.py
@@ -1,3 +1,5 @@
+from math import ceil
+
 from django.utils.decorators import decorator_from_middleware_with_args
 
 from django_s3_express_cache.middleware import CacheMiddlewareS3Compatible
@@ -10,8 +12,11 @@ def cache_page(timeout, *, cache=None, key_prefix=None):
     Works like Django's built-in `cache_page` decorator but generates cache keys
     compatible with `S3ExpressCacheBackend`.
     """
+    days = int(ceil(timeout / (60 * 60 * 24)))
+    time_based_prefix = f"{days}-days"
     return decorator_from_middleware_with_args(CacheMiddlewareS3Compatible)(
         page_timeout=timeout,
         cache_alias=cache,
         key_prefix=key_prefix,
+        time_based_prefix=time_based_prefix,
     )
